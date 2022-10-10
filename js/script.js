fa1dbbc0d61fe2407ae07f68b4ccc5284c0f8115
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-10-10 15:58:33 +0200

    end

--- js/script.js
+++ js/script.js
@@ -7,18 +7,33 @@
 
 for(i=0; i<100; i++){
 
-let a = i+1 ;   
+let a = i+1 ;
+let style_div = "style1";
+
 if (a%3==0 && a%5==0){
-    console.log("FizzBuzz");
+    style_div = "style2";
+    a = "FizzBuzz";
+    console.log(a);
 
 } else if (a%3==0){
-    console.log("Fizz");
+    style_div = "style3";
+    a = "Fizz"
+    console.log(a);
     
 }else if (a%5==0){
-    console.log("Buzz")
+    style_div = "style4";
+    a = "Buzz";
+    console.log(a);
     
 }else{
     console.log(a);
 }
+
+let div = document.createElement("div");
+div.className = style_div;
+   let singleBox = document.getElementById("box");
+   div.append(a);
+   singleBox.append(div);
+   console.log(singleBox)
 
 }
