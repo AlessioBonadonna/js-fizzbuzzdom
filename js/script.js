aae39f4c6a1b301518574ce88df71c0cdea8b4a5
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-10-10 15:15:46 +0200

    end 1 part

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-let a = 3;
-console.log(a);
+// il mio M.O
+// scrivo un ciclo for che mi stampi i numeri da 1 a 100 in console
+//If n % 3 = 0 && n % 5 = 0  print FizzBuzz
+// Else if n % 3 = 0  print Fizz
+// else if n % 5 = 0 Buzz
+// else print   Number
+
+for(i=0; i<100; i++){
+
+let a = i+1 ;   
+if (a%3==0 && a%5==0){
+    console.log("FizzBuzz");
+
+} else if (a%3==0){
+    console.log("Fizz");
+    
+}else if (a%5==0){
+    console.log("Buzz")
+    
+}else{
+    console.log(a);
+}
+
+}
